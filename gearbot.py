6c4d2434ba9cb868831e62a0ca7473e1fa39b419
estellia0 <gualandi.lorenzo@gmail.com>
2018-10-18 14:45:36 +0200

    Update gearbot.py
    Added channel restriction for !gear, changed input to !gear Family Character Lvl Class AP AWAAP DP Gear Pic, added check on url being a direct link and added check for ap/aaw/dp. Updated embed output to reflect that. Added function to remove gear by discord id. Updated sheet access according to gspread docs

--- gearbot.py
+++ gearbot.py
@@ -22,7 +22,7 @@ def read_gear_list():
         GEARdict = pickle.load(fp)
 
 #todo add custom roles or role by id
-async def is_officer(message): 
+async def is_officer(message):
     if "maids" in [y.name.lower() for y in message.author.roles]: #add your officier role name here
         return True
 
@@ -31,11 +31,13 @@ async def is_officer(message):
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('auth.json', scope)#add your json auth file name here and in the same folder
+credentials = ServiceAccountCredentials.from_json_keyfile_name('', scope)#add your json auth file name here and in the same folder
 
 gc = gspread.authorize(credentials)
 
-wks = gc.open("sheetname").sheet1 #add your sheet name here
+sh = gc.open_by_url(" ") #sheet url here
+wks = sh.worksheet("BOT") #sheet tab name here
+
 
 #reformat the message removing the bot prefix
 def format_input(prefix,message):
@@ -48,7 +50,7 @@ def get_date(message):
     date = date[0]
     return date
 
-#return the content of the msg 
+#return the content of the msg
 def get_msg_content(message):
     content = message.split(" ", 1)
     content = content[1]
@@ -66,49 +68,68 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!gear'):
-        msg = format_input("!gear", message.content) #cleanup the message
-        if message.mentions == []: #if there's no mentions it means you want to add/update gears otherwise pull the mentioned gear out
-            msg_list = msg.split(" ",3) #plit the msg in the first 3 arguments and check them one by one, futher args will be invalid, 
-            if(len(msg_list) == 3 and msg_list[0].lower() in bdo_classes and msg_list[1].isnumeric() and validators.url(msg_list[2])):
-                if msg: #will have an excpetion if one of the args is not present fix soon
-                    if message.author.id in GEARdict.keys(): #if key is already there update it
-                        del GEARdict[message.author.id]
-                        GEARdict[message.author.id].append(msg_list[0])
-                        GEARdict[message.author.id].append(msg_list[1])
-                        GEARdict[message.author.id].append(msg_list[2])
-                        write_gear_list()
+        if message.channel.id == '465920848738385920': #change channel id here
+            msg = format_input("!gear", message.content) #cleanup the message
+            if message.mentions == []: #if there's no mentions it means you want to add/update gears otherwise pull the mentioned gear out
+                msg_list = msg.split(" ",8) #split the msg in multiple args
+                if(len(msg_list) == 8 and msg_list[2].isnumeric() and msg_list[3].lower() in bdo_classes and
+                        msg_list[4].isnumeric() and msg_list[5].isnumeric() and
+                        msg_list[6].isnumeric() and validators.url(msg_list[7])):
+                    if(msg_list[7].endswith(".png") or msg_list[7].endswith(".jpg") ):
+                        if msg: #forgot why it's here
+                            if message.author.id in GEARdict.keys(): #if key is already there update it
+                                del GEARdict[message.author.id]
+                                GEARdict[message.author.id].append(msg_list[0]) #family n
+                                GEARdict[message.author.id].append(msg_list[1]) #char n
+                                GEARdict[message.author.id].append(msg_list[2]) #lvl
+                                GEARdict[message.author.id].append(msg_list[3]) #class
+                                GEARdict[message.author.id].append(msg_list[4]) #ap
+                                GEARdict[message.author.id].append(msg_list[5]) #awaap
+                                GEARdict[message.author.id].append(msg_list[6]) #dp
+                                GEARdict[message.author.id].append(msg_list[7]) #pic
+                                write_gear_list()
+                                await client.send_message(message.channel,
+                                                      "Your gear has been updated!")
+                            else: #else add it anew
+                                GEARdict[message.author.id].append(msg_list[0])
+                                GEARdict[message.author.id].append(msg_list[1])
+                                GEARdict[message.author.id].append(msg_list[2])
+                                GEARdict[message.author.id].append(msg_list[3])
+                                GEARdict[message.author.id].append(msg_list[4])
+                                GEARdict[message.author.id].append(msg_list[5])
+                                GEARdict[message.author.id].append(msg_list[6])
+                                GEARdict[message.author.id].append(msg_list[7])
+                                write_gear_list()
+                                await client.send_message(message.channel,
+                                                  "Your gear has been added ")
+                    else:
                         await client.send_message(message.channel,
-                                              "Your gear has been updated!")
-                    else: #else add it anew
-                        GEARdict[message.author.id].append(msg_list[0])
-                        GEARdict[message.author.id].append(msg_list[1])
-                        GEARdict[message.author.id].append(msg_list[2])
-                        write_gear_list()
-                        await client.send_message(message.channel,
-                                          "Your gear has been added ")
-            else:
-                await client.send_message(message.channel, "Use !gear + class + GS + gear pic img (For Dark Knight use dk)")
-        else:
-            if msg:
-                id = message.mentions[0].id
-                for key in GEARdict:
-                    if key == id:
-                        userID = await client.get_user_info(key)
-                        url = GEARdict[key]
-                        picurl = url[2].strip()
-                        stringfix = url[0] + " "+ url[1] #do some hacky shit to get the correct string and print in a nice embed
-                        classgs = stringfix.strip()
-                        embed = discord.Embed()
-                        embed.set_author(name=userID,icon_url=message.mentions[0].avatar_url)
-                        embed.set_thumbnail(url=message.mentions[0].avatar_url)
-                        embed.add_field(name=message.mentions[0].display_name,value=classgs,inline=False)
-                        embed.set_image(url=picurl)
-                        await client.send_message(message.channel,embed=embed)
-                        break;
+                                                  "Use a direct link to the picture (url must end with.png/.jpg) use ShareX it's free")
                 else:
-                   await client.send_message(message.channel,"Gear not found!")
+                    await client.send_message(message.channel, "Use !gear Family Character Lvl Class AP AWAAP DP Gear Pic link "
+                                                               "(For Dark Knight use dark-knight)")
             else:
-                await client.send_message(message.channel, "Use !gear + @someone!")
+                if msg:
+                    id = message.mentions[0].id
+                    for key in GEARdict:
+                        if key == id:
+                            userID = await client.get_user_info(key)
+                            list = GEARdict[key]
+                            picurl = list[7].strip()
+                            gs = int(((int(list[4]) + int(list[5])) / 2) + int(list[6]))
+                            stringfix = list[0] + " "+ list[1] + " " + " Lv "+ list[2] + " " + str(gs) 
+                            classgs = stringfix.strip()
+                            embed = discord.Embed()
+                            embed.set_author(name=userID,icon_url=message.mentions[0].avatar_url)
+                            embed.set_thumbnail(url=message.mentions[0].avatar_url)
+                            embed.add_field(name=message.mentions[0].display_name,value=classgs,inline=False)
+                            embed.set_image(url=picurl)
+                            await client.send_message(message.channel,embed=embed)
+                            break
+                    else:
+                       await client.send_message(message.channel,"Gear not found!")
+                else:
+                    await client.send_message(message.channel, "Use !gear + @someone!")
 
     elif message.content.startswith('!remove'):
         eval = await is_officer(message)
@@ -119,46 +140,76 @@ async def on_message(message):
                     del GEARdict[key]
                     write_gear_list()
                     await client.send_message(message.channel, "Gear has been removed")
-                    print("gear removed")
-                    break;
+                    break
             else:
                 await client.send_message(message.channel, "Gear not found!")
-        else:  
+        else:
             await client.send_message(message.channel,
                                       "You ain't a maid!")
 
+    elif message.content.startswith('!rmid'): #remove gear by discord id
+        eval = await is_officer(message)
+        if eval:
+            id = format_input("!rmid", message.content)
+            for key in GEARdict:
+                if key == id:
+                    del GEARdict[key]
+                    write_gear_list()
+                    await client.send_message(message.channel, "Gear has been removed")
+                    break
+            else:
+                await client.send_message(message.channel, "Gear not found!")
+        else:
+            await client.send_message(message.channel,
+                                      "You ain't a maid!")
 
-
-    elif message.content.startswith('!sheet'): #this is very hacky but it works 
+    elif message.content.startswith('!sheet'): #this is very hacky but it works
         eval = await is_officer(message)
         if eval:
             i = 0
             gc.login() #refresh auth token
-            cell_name_list = wks.range('A2:A70') #init enough lists to fill the sheet later
-            cell_class_list = wks.range('B2:B70')
-            cell_gs_list = wks.range('C2:C70')
-            cell_gearpic_list = wks.range('D2:D70')
+            cell_name_list = wks.range('B4:B104') #init enough lists to fill the sheet later
+            cell_family_list = wks.range('C4:C104')
+            cell_character_list = wks.range('D4:D104')
+            cell_lvl_list = wks.range('E4:E104')
+            cell_class_list = wks.range('F4:F104')
+            cell_ap_list = wks.range('G4:G104')
+            cell_awaap_list = wks.range('H4:H104')
+            cell_dp_list = wks.range('I4:I104')
+            cell_gearpic_list = wks.range('K4:K104')
             for key in GEARdict.fromkeys(GEARdict): #parse tru every key in the map and convert the id to real username then append it to the cell lists
                 user = await client.get_user_info(key)
                 new_key = user.name
                 cell_name_list[i].value = new_key
                 infos = GEARdict[key]
-                cell_class_list[i].value = infos[0]
-                cell_gs_list[i].value = infos[1]
-                cell_gearpic_list[i].value = infos[2]
+                cell_family_list[i].value = infos[0]
+                cell_character_list[i].value = infos[1]
+                cell_lvl_list[i].value = infos[2]
+                cell_class_list[i].value = infos[3]
+                cell_ap_list[i].value = infos[4]
+                cell_awaap_list[i].value = infos[5]
+                cell_dp_list[i].value = infos[6]
+                cell_gearpic_list[i].value = infos[7]
+
                 i += 1
             try: #write the lists to the sheet
                 wks.update_cells(cell_name_list)
+                wks.update_cells(cell_family_list)
+                wks.update_cells(cell_character_list)
+                wks.update_cells(cell_lvl_list)
                 wks.update_cells(cell_class_list)
-                wks.update_cells(cell_gs_list)
+                wks.update_cells(cell_ap_list)
+                wks.update_cells(cell_awaap_list)
+                wks.update_cells(cell_dp_list)
                 wks.update_cells(cell_gearpic_list)
+
                 await client.send_message(message.channel, "Gear updated on the sheet!")
             except:
                 await client.send_message(message.channel, "API Error") #most likely issues with the token
-        else:  
+        else:
             await client.send_message(message.channel,
                                       "You ain't a maid!")
-	elif message.content.startswith('!check'): #self explanatory just counts how many entries we have got so far
+    elif message.content.startswith('!check'): #self explanatory just counts how many entries we have got so far
         eval = await is_officer(message)
         if eval:
             i = 0
@@ -167,7 +218,7 @@ async def on_message(message):
                 i += 1
             else:
                 await client.send_message(message.channel, "Number of users that submitted their gear: " + i)
-        else:  
+        else:
             await client.send_message(message.channel,
                                       "You ain't a maid!")
 
